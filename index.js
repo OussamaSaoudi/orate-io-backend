b93533ee1e9383e5e04a62444709df0cbe775e1d
Oussama Saoudi <oussama.sa216@gmail.com>
2021-06-19 13:36:37 -0400

    Added ESLint support for JSDoc
    Added dev dependency eslint-plugin-jsdoc and added recommended
    rules to eslint.
    
    Fixed eslint.rc and index.js to include @file descriptions
    
    Signed-off-by: Oussama Saoudi <oussama.sa216@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * @file The index.js file is the root of the program runtime and
+ * deals with server and environment variables.
+ */
 const http = require('http')
 const app = http.createServer((request, response) => {
   response.writeHead(200, { 'Contetn-Type': 'text/plain' })
