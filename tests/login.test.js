f80caf749e8677ec1e4c66635f78189a11713cbe
Yacine Saoudi <yac.sao453@gmail.com>
2021-06-28 14:24:59 -0400

    test fix

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,33 +1,23 @@
+/**
+ * @file Test file.
+ * @author Yacine Saoudi
+ */
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app.js')
 /*
- * const mongoose = require('mongoose')
- * const supertest = require('supertest')
- * const app = require('../app.js')
  * const loginRouter=require('../controllers/login.js')
  * const api = supertest(app)
  */
 
-/*
- * const initialAccounts = [
- * ]
- */
+const initialAccounts = [
+]
 
-/*
- * test('initial accounts is empty', async () => {
- *   expect(initialAccounts).toHaveLength(0)
- * })
- */
-
-/*
- * test('send post request to login', async () => {
- *   await api
- *     .post('/login', { username: 'cringe', password:'cool' })
- *     .expect(200)
- * })
- */
+test('initial accounts is empty', async () => {
+  expect(initialAccounts).toHaveLength(0)
+})
 
 
-/*
- * afterAll(() => {
- *   mongoose.connection.close()
- * })
- */
+afterAll(() => {
+  mongoose.connection.close()
+})
